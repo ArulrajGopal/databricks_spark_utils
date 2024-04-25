f46a22e4e5448d0abf9c53c7150a7f328956dd4d
ArulrajGopal <122370244+ArulrajGopal@users.noreply.github.com>
2024-04-25 16:22:49 +0530

    Update TakeVersionSnap_UnityCatalog_Tables.py

--- TakeVersionSnap_UnityCatalog_Tables.py
+++ TakeVersionSnap_UnityCatalog_Tables.py
@@ -15,3 +15,16 @@ def get_version (tbl_name):
 
 def get_count (tbl_name):
 	return spark.sql(f"select count(*) from {tbl_name}").collect()[0][0]
+
+final_data_lst =[]
+for tbl in tbl_lst_df.rdd.collect():
+	table_name = tbl[0]
+	version = get_version(table_name)
+	row_count = get_count(table_name)
+	lst = [table_name, version, row_count]
+	final_data_lst.append(lst)
+
+columns = ["TableName","Version","RowCount"]
+spark.createDataFrame(final_data_lst, columns).display()
+
+
