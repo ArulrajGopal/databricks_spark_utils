517a22e335ebecc3d9b4d1a34645bec7d628834f
ArulrajGopal <122370244+ArulrajGopal@users.noreply.github.com>
2024-04-25 16:17:21 +0530

    Update TakeVersionSnap_UnityCatalog_Tables.py

--- TakeVersionSnap_UnityCatalog_Tables.py
+++ TakeVersionSnap_UnityCatalog_Tables.py
@@ -6,27 +6,12 @@ catalog_name = "my_catalog"
 schema_name = "my_schema"
 catalog_schema_name = catalog_name+"."+schema_name
 
-df = spark.sql(f"show tables from {catalog_schema_name}")
-dfl = df.withColumn ("Table", concat (lit (catalog_name), lit("."), col("database"), lit("."), col ("tablename"))) 
+tbl_lst_df = spark.sql(f"show tables from {catalog_schema_name}")\
+			.withColumn("Table",concat(lit(catalog_name),lit("."),col("database"),lit("."),col("tablename")))\
+			.select("Table")
 
-df2 = dfl.select("Table")
-tbl_1st = df2.rdd.map(lambda x : x.Table).collect()
+def get_version (tbl_name):
+	return spark.sql(f"desc history {tbl_name}").orderBy(desc("version")).select("version").limit(1).collect()[0][0]
 
-final_list = []
-for tbl in tbl_1st:
-	df4=spark.sql(f"desc history (tbl)")\
-					.orderBy(desc("version")).select("version").limit(1)\
-					.withColumn ("tablename", lit (tbl))
-	run_detail = df4.withColumn ("Details", concat(col ("tablename"),lit("#"),col("version")))\ 
-					.select("Details")\
-					.rdd.map(lambda x : x.Details)\
-					.collect()
-			
-	final_list.append(run_detail)
-			
-			
-
-schema = StructType([StructField("TableDetails", StringType(), True)])
-
-intiate_df = spark.createDataFrame (final_list, schema)
-intiate_df.display()
+def get_count (tbl_name):
+	return spark.sql(f"select count(*) from {tbl_name}").collect()[0][0]
