9b2e6ea9944966ecfdd661c792214a5872a9a543
ArulrajGopal <ArulrajGopal>
2024-08-05 12:27:02 +0000

    update

--- pyspark_joins.py
+++ pyspark_joins.py
@@ -20,8 +20,8 @@ record_6 = [1, 'A','Engineer']
 record_7 = [2, 'A', 'doctor']
 record_8 = [2,'B', 'lawyer']
 
-list = [record_1, record_2, record_3,record_4,record_5,record_6]
-list_2 = [record_6, record_7, record_8]
+list1 = [record_1, record_2, record_3,record_4,record_5]
+list2 = [record_6, record_7, record_8]
 
 df_schema = StructType(fields=[StructField("sr_no", IntegerType(), False),
                                StructField("section", StringType(), False),
@@ -34,8 +34,8 @@ df_2_schema = StructType(fields=[StructField("sr_no", IntegerType(), False),
                                 StructField("profession", StringType(), True),     
 ])
 
-df = spark.createDataFrame(list, df_schema)
-df_2 = spark.createDataFrame(list_2, df_2_schema)
+df = spark.createDataFrame(list1, df_schema)
+df_2 = spark.createDataFrame(list2, df_2_schema)
 
 # COMMAND ----------
 
@@ -45,44 +45,19 @@ df_2.show()
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC #Join with Single condition
+# MAGIC #Joining with Multiple condition
 
 # COMMAND ----------
 
 joined_df = df.alias('LH')\
-                .join(df_2.alias('RH'), (col('LH.sr_no') == col('RH.sr_no')) & (col('LH.section') == col('RH.section')) , 'inner')\
+                .join(df_2.alias('RH'), (col('LH.sr_no') == col('RH.sr_no')) & (col('LH.section') == col('RH.section')) , 'left')\
                 .select('LH.*','RH.profession')
 
 joined_df.show()
 
 # COMMAND ----------
 
-
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC #Joining with Multiple condition
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-
+joined_df.explain(True)
 
 # COMMAND ----------
 
@@ -91,12 +66,31 @@ joined_df.show()
 
 # COMMAND ----------
 
+from pyspark.sql.functions import broadcast
 
+# COMMAND ----------
 
-# Syntax for broad cast join?
-# Join_cond = (
-# (col(“A”) == col(“B”)) & 
-# (col(“C”) == col(“D”)) &
-# )
-# New_df = df.alias(“LH”).join(broadcast(df1).alias(“RH”), Join_cond
-# , “left”)
+large_df = df
+small_df = df_2
+
+result_df = large_df.alias("LH").join(broadcast(small_df.alias("RH")), (col('LH.sr_no') == col('RH.sr_no')) & (col('LH.section') == col('RH.section')), "left")
+result_df.display()
+# Note: - this is not large enough to broadcast, but for demo purpose, it has been done.
+
+result_df.explain(True)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #Auto Broadcast
+
+# COMMAND ----------
+
+spark.conf.get("spark.sql.autoBroadcastJoinThreshold")
+
+# COMMAND ----------
+
+# To disable autoBroadcastJoin >> set -1
+# By default it is 10485760 i.e. 10MB
+spark.conf.set("spark.sql.autoBroadcastJoinThreshold", -1)
+spark.conf.get("spark.sql.autoBroadcastJoinThreshold")
