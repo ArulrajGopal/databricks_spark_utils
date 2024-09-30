38768d13187785d79de08afd375b22fa26451b78
ArulrajGopal <122370244+ArulrajGopal@users.noreply.github.com>
2024-10-01 00:49:52 +0530

    Update base_file.py

--- base_file.py
+++ base_file.py
@@ -1,7 +1,7 @@
-1. read a file into dataframe
-2. rename few columns (using selectExpr)
-3. join with parent table, which should have atlease two joining key and get 1 or two key
-4. rename those columns using withColumn
-5. do many aggregations
-5. do windows functions
-6. write it some where
+# 1. read a file into dataframe
+# 2. rename few columns (using selectExpr)
+# 3. join with parent table, which should have atlease two joining key and get 1 or two key
+# 4. rename those columns using withColumn
+# 5. do many aggregations
+# 6. do windows functions
+# 7. write it some where
